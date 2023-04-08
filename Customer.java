7dac9ac1fb224241454ea4dcace4350aedc024fb
williso16 <129903243+williso16@users.noreply.github.com>
2023-04-08 16:37:05 -0400

    Update Customer.java

--- Customer.java
+++ Customer.java
@@ -5,116 +5,38 @@ import java.util.HashMap;
 public class Customer {
     String username;
     String password;
-    String ID;  // COULD BE INT
+    String id;  // COULD BE INT
+    String name;
     ArrayList<Product> shoppingCart;
 
     ArrayList<Product> purchased;
 
-    public Customer(String username, String password, String ID) {  // Constructor for if customer exists
+    public Customer(String username, String password, String id, String name) {  // Constructor for if customer exists
         this.username = username;
         this.password = password;
-        this.ID = ID;
+        this.id = id;
+        this.name = name;
     }
 
     public void close() {
         // TODO: This should write shopping cart and purchase history when called.  Read from the fields and write to the files
     }
 
-    public void addNewCustomer(String fileName) {
-        String info = String.format("%s,%s,%s\n", this.username, this.password, this.ID);
-        String prev = "";
-        String s;
+    public void addAccount(String fileName) {  // FIXME -> add Account information to the database - could be easier to hard code the database file instead of using fileName parameter
+        String info = String.format("%s,%s,%s,%s", this.id, this.username, this.password, this.name);
 
-        try (BufferedReader bfr = new BufferedReader(new FileReader(fileName))) {
-            while (true) {
-                s = bfr.readLine();
-                if (s == null) {
-                    break;
-                }
-                prev += s + "\n";
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        // TODO: Should add the info string to the database
 
-        try (PrintWriter pw = new PrintWriter(fileName)) {
-            pw.println(prev + info);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 
-    public void saveCart(String fileName) {  // FIXME -> This method uses lots of for loops, may be slow
-        // Idea is that carts will be linked to users ID numbers in a shopping cart file
-        // Can use hashmaps to take advantage of that
-        // Link the User ID with the items in the cart -> would probably have to find a good way to store all of the
-        // information -> or if products had ID's just link a customer ID w/ product ID's
+    public void saveCart(String fileName) {  // FIXME -> Integrate Databases
+        // Want to write the users cart into the cart file for database
+        // Information stored in arraylist of products (shoppingCart field)
+    }
 
-
-
-        String prev = "";
-        String s;
-        String[] tempCart;
-        String tempStr;
-        ArrayList<String> keys = new ArrayList<>();
-
-
-        HashMap<String, String> shoppingCarts = new HashMap<>();
-
-        try (BufferedReader bfr = new BufferedReader(new FileReader(fileName))) {
-            while (true) {
-                tempStr = "";
-                s = bfr.readLine();
-                if (s == null) {
-                    break;
-                }
-                tempCart = s.split(",");
-                keys.add(tempCart[0]);
-
-                for (int i = 1; i < tempCart.length; i++) {
-                    if (i == tempCart.length - 1) {
-                        tempStr += tempCart[i];
-                    } else {
-                        tempStr += tempCart[i] + ",";
-                    }
-                }
-                shoppingCarts.put(tempCart[0], tempStr);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-
-        // Gets the new cart based on what user added
-        String newCart = "";
-        for (int i = 0; i < this.shoppingCart.size(); i++) {
-            if (i == this.shoppingCart.size() - 1) {
-                newCart += this.shoppingCart.get(i).name;  // Adds product name (+ "," if not last item)
-            } else {
-                newCart += this.shoppingCart.get(i).name + ",";
-            }
-        }
-
-
-        // Assigning or replacing items related to the key corresponding to the user
-        String prevCart;
-        try {
-            prevCart = shoppingCarts.get(this.ID);
-            shoppingCarts.replace(this.ID, prevCart + newCart);
-        } catch (Exception e) {
-            keys.add(this.ID);
-            shoppingCarts.put(this.ID, newCart);
-        }
-
-
-
-        try (PrintWriter pw = new PrintWriter(fileName)) {
-            for (String key: keys) {
-                pw.printf("%s,%s\n", key, shoppingCarts.get(key));
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    public void saveHistory(String fileName) {  // FIXME -> Integrate Databases
+        // want to write the users history into a history file in database
+        // Information stored in arraylist of products (purchased field)
     }
 
     public void addToCart(Product product) {
@@ -141,8 +63,8 @@ public class Customer {
         return password;
     }
 
-    public String getID() {
-        return ID;
+    public String getId() {
+        return id;
     }
 
     public void setUsername(String username) {
@@ -153,16 +75,53 @@ public class Customer {
         this.password = password;
     }
 
+    public void addToHistory(Product product) {
+        this.purchased.add(product);
+    }
+
+    public void setPurchased(ArrayList<Product> purchased) {
+        this.purchased = purchased;
+    }
+
+    public ArrayList<Product> getPurchased() {
+        return purchased;
+    }
+
+    public void removeFromHistory(Product product) {
+        this.purchased.remove(product);
+    }
 
 }
 
 class Product {
-    String name;
-    public Product(String name) {
+    private String storeID;
+    private String id;
+    private String name;
+    private String description;
+    private int quantity;
+    private double price;
+    private String seller;
+
+    public Product(String id, String name, String description, int quantity, double price, String seller) {
+        this.id = id;
         this.name = name;
+        this.description = description;
+        this.quantity = quantity;
+        this.price = price;
+        this.seller = seller;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public int getQuantity() {
+        return this.quantity;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s,%s,%s,%s,%s,%s", id, name, storeID, description, quantity, price);
     }
 }
 
-class Seller {
-
-}
